8d152baec5141027b692f3b9fd425e26e840342b
Pedro Agustin Vega <pedroagustinvega@proton.me>
2025-11-21 15:53:53 -0300

    feat: implement navigation layout and java code updated

--- src/constants/javaCode.ts
+++ src/constants/javaCode.ts
@@ -30,13 +30,22 @@ public void keyPressed(KeyEvent e) {
     switch (e.getKeyCode()) {
         case 39: {
             if (ThreadsController.directionSnake == 2) break;
-            ThreadsController.directionSnake = 1;
-            break;
-        }
-          
+                ThreadsController.directionSnake = 1;
+                break;
+            }
         case 38: {
             if (ThreadsController.directionSnake == 4) break;
-            ThreadsController.directionSnake = 3;
+                ThreadsController.directionSnake = 3;
+                break;
+            }
+        case 37: {
+            if (ThreadsController.directionSnake == 1) break;
+            ThreadsController.directionSnake = 2;
+            break;
+        }
+        case 40: {
+            if (ThreadsController.directionSnake == 3) break;
+            ThreadsController.directionSnake = 4;
             break;
         }
     }
