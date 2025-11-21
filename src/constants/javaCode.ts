525e716de743ad60db2bae8332b7e0ec767e6fdf
Pedro Agustin Vega <pedroagustinvega@proton.me>
2025-11-21 16:00:11 -0300

    java original code indentation fixed

--- src/constants/javaCode.ts
+++ src/constants/javaCode.ts
@@ -30,14 +30,14 @@ public void keyPressed(KeyEvent e) {
     switch (e.getKeyCode()) {
         case 39: {
             if (ThreadsController.directionSnake == 2) break;
-                ThreadsController.directionSnake = 1;
-                break;
-            }
+            ThreadsController.directionSnake = 1;
+            break;
+        }
         case 38: {
             if (ThreadsController.directionSnake == 4) break;
-                ThreadsController.directionSnake = 3;
-                break;
-            }
+            ThreadsController.directionSnake = 3;
+            break;
+        }
         case 37: {
             if (ThreadsController.directionSnake == 1) break;
             ThreadsController.directionSnake = 2;
